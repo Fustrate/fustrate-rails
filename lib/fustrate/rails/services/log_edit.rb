9304c07503a4f157f5b27fd78752efb9b6104e1b
Steven Hoffman <git@fustrate.com>
2018-09-24 13:12:15 -0700

    Use introspection and crap like that to log edits

--- lib/fustrate/rails/services/log_edit.rb
+++ lib/fustrate/rails/services/log_edit.rb
@@ -4,10 +4,8 @@ module Fustrate
   module Rails
     module Services
       module LogEdit
-        IGNORE_COLUMNS = %i[updated_at].freeze
-        DATE_FIELDS = [].freeze
-        DATETIME_FIELDS = [].freeze
-        BOOLEAN_FIELDS = [].freeze
+        IGNORE_COLUMNS = %w[id created_at updated_at].freeze
+        RENAME_COLUMNS = {}.freeze
 
         def call(subject, force: false, note: nil)
           # Make sure any before_validation callbacks are run
@@ -18,10 +16,11 @@ module Fustrate
           @note = note
           @data = subject.changes.except(*self.class::IGNORE_COLUMNS)
 
-          process_timestamps
-          process_booleans
+          process_changes
 
-          process
+          RENAME_COLUMNS.each do |from, to|
+            @data[to] = @data.delete from if @data[from]
+          end
 
           record_edit
         end
@@ -39,37 +38,87 @@ module Fustrate
           log_edit_on.events.new(
             type: 'Edited',
             user: Current.user,
-            data: @data,
+            data: { changes: @data, raw_changes: @subject.changes },
             note: @note
           )
         end
-        
+
         # Allow edit data to be recorded on a model that isn't @subject
         def log_edit_on
           @subject
         end
 
-        def process; end
-
-        # Format fields that are timestamps
-        def process_timestamps
-          self.class::DATE_FIELDS
-            .select { |key| @data[key] }
-            .each do |key|
-              @data[key] = format_timestamps(@data[key], time: false)
-            end
-
-          self.class::DATETIME_FIELDS
-            .select { |key| @data[key] }
-            .each do |key|
-              @data[key] = format_timestamps(@data[key], time: true)
-            end
+        def process_changes
+          process_datetime_columns
+          process_date_columns
+          process_boolean_columns
+          process_relations
         end
 
-        def process_booleans
-          self.class::BOOLEAN_FIELDS
-            .select { |key| @data[key] }
-            .each { |key| @data[key] = @data[key][1] ? %w[No Yes] : %w[Yes No] }
+        def process_datetime_columns
+          Array(columns[:date]).select { |col| @data[col] }.each do |col|
+            @data[col] = format_timestamps(@data[col], time: false)
+          end
+        end
+
+        def process_date_columns
+          Array(columns[:datetime]).select { |col| @data[col] }.each do |col|
+            @data[col] = format_timestamps(@data[col], time: true)
+          end
+        end
+
+        def process_boolean_columns
+          Array(columns[:boolean]).select { |col| @data[col] }.each do |col|
+            @data[col] = @data[col][1] ? %w[No Yes] : %w[Yes No]
+          end
+        end
+
+        def process_relations
+          @subject.class.reflect_on_all_associations.each do |relation|
+            if relation.options[:polymorphic]
+              process_polymorphic_relation(relation.name)
+            elsif relation.is_a?(ActiveRecord::Reflection::BelongsToReflection)
+              process_belongs_to_relation(relation.name)
+            end
+          end
+        end
+
+        def process_polymorphic_relation(name)
+          return unless @data["#{name}_id"] || @data["#{name}_type"]
+
+          @data.delete("#{name}_id")
+          @data.delete("#{name}_type")
+
+          # The type, and in rare cases the ID, may not have actually changed
+          old_id = @subject.send("#{name}_id_in_database")
+          old_type = @subject.send("#{name}_type_in_database")
+
+          @data[relation.name] = [
+            Object.const_get(old_type).find(old_id)&.to_s,
+            @subject.send(name)&.to_s
+          ]
+        end
+
+        def columns
+          # .reject { |col| col.name.match?(/_(?:type|id)\z/) }
+          @columns ||= @subject.class.columns
+            .reject { |col| IGNORE_COLUMNS.include? col.name }
+            .group_by { |column| column.sql_type_metadata.type }
+            .map { |type, cols| [type, cols.map { |col| col.name.to_sym }] }
+            .to_h
+        end
+
+        def process_belongs_to_relation(name)
+          return unless @data["#{name}_id"]
+
+          new_value = @subject.send(name)
+
+          change = @data.delete "#{name}_id"
+
+          @data[name] = [
+            (new_value.class.find(change[0])&.to_s if change[0]),
+            new_value&.to_s
+          ]
         end
 
         def format_timestamps(datetimes, time: true)
@@ -78,12 +127,6 @@ module Fustrate
 
             timestamp.strftime(time ? '%-m/%-d/%y %-I:%M %p' : '%-m/%-d/%y')
           end
-        end
-
-        def find_polymorphic_name(type, id)
-          return unless type && id
-
-          Object.const_get(type).find(id).to_s
         end
       end
     end
