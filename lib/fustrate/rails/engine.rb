37744c8e5d8b71ddb5ad920ec47f90f43515e95f
Steven Hoffman <git@fustrate.com>
2020-06-05 12:51:35 -0700

    require files
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/fustrate/rails/engine.rb
+++ lib/fustrate/rails/engine.rb
@@ -5,6 +5,9 @@
 
 require 'rails/engine'
 
+require 'fustrate/rails/concerns/clean_attributes'
+require 'fustrate/rails/concerns/sanitize_html'
+
 require 'fustrate/rails/services/base'
 require 'fustrate/rails/services/generate_csv'
 require 'fustrate/rails/services/generate_excel'
