eaebeecc103095b4f962397550afed794acf057a
Steven Hoffman <fustrate@me.com>
2022-04-08 11:37:59 -0700

    LogEdit changes
    Make `user` configurable
    Change `data` to `changes`
    
    Signed-off-by: Steven Hoffman <fustrate@me.com>

--- lib/fustrate/rails/services/log_edit.rb
+++ lib/fustrate/rails/services/log_edit.rb
@@ -10,19 +10,21 @@ module Fustrate
         IGNORE_COLUMNS = %w[id created_at updated_at].freeze
         RENAME_COLUMNS = {}.freeze
 
-        def call(subject, force: false, note: nil)
+        attr_reader :subject, :force, :note, :user
+
+        def call(subject, force: false, note: nil, user: nil)
           # Make sure any before_validation callbacks are run
           subject.validate
 
           @subject = subject
           @force = force
           @note = note
-          @data = subject.changes.except(*self.class::IGNORE_COLUMNS)
+          @user = user || ::Current.user
 
           process_changes
 
           self.class::RENAME_COLUMNS.each do |from, to|
-            @data[to] = @data.delete from if @data[from]
+            changes[to] = changes.delete(from) if changes[from]
           end
 
           record_edit
@@ -30,28 +32,29 @@ module Fustrate
 
         protected
 
-        def record_edit
-          # Get rid of changes from nil to '' and whatnot
-          @data.delete_if { |_, values| values.all?(&:blank?) || values[0] == values[1] }
-
-          return if @data.empty? && !@force
-
-          log_edit_on_relation.new(
-            type: 'Edited',
-            user: ::Current.user,
-            data: { changes: @data, raw_changes:, **additional_data },
-            note: @note
-          )
+        def changes
+          @changes ||= subject.changes.except(*self.class::IGNORE_COLUMNS)
         end
 
-        # Allow edit data to be recorded on a model that isn't @subject
-        def log_edit_on = @subject
+        def record_edit
+          # Get rid of changes from nil to '' and whatnot
+          changes.delete_if { |_, values| values.all?(&:blank?) || values[0] == values[1] }
+
+          return if changes.empty? && !force
+
+          log_edit_on_relation.new(type: 'Edited', user:, note:, data: edit_data)
+        end
+
+        # Allow edits to be recorded on a model that isn't `subject`
+        def log_edit_on = subject
 
         def log_edit_on_relation = log_edit_on.events
 
+        def edit_data = { changes:, raw_changes:, **additional_data }
+
         def additional_data = {}
 
-        def raw_changes = @subject.changes
+        def raw_changes = subject.changes
 
         def process_changes
           process_datetime_columns
@@ -62,28 +65,28 @@ module Fustrate
 
         def process_datetime_columns
           columns_of_type(:date).each do |col|
-            @data[col] = format_timestamps(@data[col], time: false)
+            changes[col] = format_timestamps(changes[col], time: false)
           end
         end
 
         def process_date_columns
           columns_of_type(:datetime).each do |col|
-            @data[col] = format_timestamps(@data[col], time: true)
+            changes[col] = format_timestamps(changes[col], time: true)
           end
         end
 
         def process_boolean_columns
           columns_of_type(:boolean).each do |col|
-            @data[col] = @data[col][1] ? %w[No Yes] : %w[Yes No]
+            changes[col] = changes[col][1] ? %w[No Yes] : %w[Yes No]
           end
         end
 
         def columns_of_type(type)
-          Array(columns[type]).select { |col| @data[col] }
+          Array(columns[type]).select { |col| changes[col] }
         end
 
         def process_relations
-          @subject.class.reflect_on_all_associations.each do |relation|
+          subject.class.reflect_on_all_associations.each do |relation|
             if relation.options[:polymorphic]
               process_polymorphic_relation(relation.name)
             elsif relation.is_a?(::ActiveRecord::Reflection::BelongsToReflection)
@@ -93,39 +96,40 @@ module Fustrate
         end
 
         def process_polymorphic_relation(name)
-          return unless @data["#{name}_id"] || @data["#{name}_type"]
+          return unless changes["#{name}_id"] || changes["#{name}_type"]
 
-          @data.delete("#{name}_id")
-          @data.delete("#{name}_type")
+          changes.delete("#{name}_id")
+          changes.delete("#{name}_type")
 
+          changes[name] = [find_old_polymorphic_record(name), subject.__send__(name)&.to_s]
+        end
+
+        def find_old_polymorphic_record(name)
           # The type, and in rare cases the ID, may not have actually changed
-          old_id = @subject.__send__("#{name}_id_in_database")
-          old_type = @subject.__send__("#{name}_type_in_database").presence
+          old_id = subject.__send__("#{name}_id_in_database")
+          old_type = subject.__send__("#{name}_type_in_database").presence
 
-          @data[name] = [
-            (::Object.const_get(old_type).find(old_id)&.to_s if old_type),
-            @subject.__send__(name)&.to_s
-          ]
+          ::Object.const_get(old_type).find(old_id)&.to_s if old_type
         end
 
         def columns
           # .reject { |col| col.name.match?(/_(?:type|id)\z/) }
-          @columns ||= @subject.class.columns
+          @columns ||= subject.class.columns
             .reject { |col| self.class::IGNORE_COLUMNS.include? col.name }
             .group_by { |column| column.sql_type_metadata.type }
             .transform_values { |cols| cols.map { |col| col.name.to_sym } }
         end
 
         def process_belongs_to_relation(name)
-          return unless @data["#{name}_id"]
+          return unless changes["#{name}_id"]
 
-          new_value = @subject.__send__(name)
+          new_value = subject.__send__(name)
 
-          old_value_id = @data.delete("#{name}_id")[0]
+          old_value_id = changes.delete("#{name}_id")[0]
 
           # If we're removing the value, new_value.class will be nil, so we need to use reflection.
-          @data[name] = [
-            (@subject.class.reflect_on_association(name).klass.find(old_value_id)&.to_s if old_value_id),
+          changes[name] = [
+            (subject.class.reflect_on_association(name).klass.find(old_value_id)&.to_s if old_value_id),
             new_value&.to_s
           ]
         end
