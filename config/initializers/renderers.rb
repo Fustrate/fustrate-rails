d919cd67460b9a14f21e0bc0c27cbd04a4b412cf
Steven Hoffman <git@fustrate.com>
2023-03-20 16:57:58 -0700

    refactor renderer code, add zip renderer

--- config/initializers/renderers.rb
+++ config/initializers/renderers.rb
@@ -6,15 +6,20 @@
 ::ActionController::Renderers.add :excel do |data, options|
   name = options[:filename] || 'export'
 
-  data_to_send = case data
-                 when ::Axlsx::Package then data.to_stream.read
-                 when ::StringIO then data.read
-                 when ::Array then ::Fustrate::Rails::Services::GenerateExcel.new.call(data, options[:sheet] || name)
-                 else
-                   data
-                 end
+  render_options = { filename: "#{name}.xlsx", disposition: 'attachment', type: :xlsx }
 
-  send_data(data_to_send, filename: "#{name}.xlsx", disposition: 'attachment', type: :xlsx)
+  case data
+  when ::Axlsx::Package
+    send_data(data.to_stream.read, **render_options)
+  when ::Array
+    send_data(::Fustrate::Rails::Services::GenerateExcel.new.call(data, options[:sheet] || name), **render_options)
+  when ::Pathname
+    render_options[:filename] = data.basename.to_s if render_options[:filename] == 'export'
+
+    send_file(data, **render_options)
+  else
+    send_data(data, **render_options)
+  end
 end
 
 ::ActionController::Renderers.add :csv do |data, options|
@@ -24,6 +29,10 @@ end
     ::Fustrate::Rails::Services::GenerateCsv.new.call(data),
     filename: "#{name}.csv",
     disposition: 'attachment',
-    type: 'text/csv'
+    type: :csv
   )
+end
+
+::ActionController::Renderers.add :zip do |path, options|
+  send_file path, type: :zip, filename: "#{options[:filename]}.zip"
 end
