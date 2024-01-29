11da6f8b0881e4c0f3a11e9a8822f31877b8d0a7
Steven Hoffman <git@fustrate.com>
2024-01-29 10:40:04 -0800

    don’t require deleted files

--- lib/fustrate/rails/engine.rb
+++ lib/fustrate/rails/engine.rb
@@ -10,8 +10,6 @@ require 'fustrate/rails/concerns/model'
 require 'fustrate/rails/concerns/sanitize_html'
 
 require 'fustrate/rails/services/base'
-require 'fustrate/rails/services/generate_csv'
-require 'fustrate/rails/services/generate_excel'
 require 'fustrate/rails/services/log_edit'
 
 module Fustrate
